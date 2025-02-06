cc054c7eec77a8a8cc4034076193900a60dd5e8a
naveenjha031 <naveenjha344@gmail.com>
2025-02-06 10:39:00 +0530

    [Naveen] Add. FirstNegativeNumber Problem

---
+++ linearsearch/FirstNegativeNumber.java
@@ -0,0 +1,19 @@
+package org.example.linearsearch;
+
+public class FirstNegativeNumber {
+
+    public static int linear(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        System.out.println(linear(arr));
+
+    }
+}
